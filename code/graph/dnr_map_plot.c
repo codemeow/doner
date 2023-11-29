b97b63d15cce9e5ba950267b55766390a3f69a67
codemeow <alexcones@gmail.com>
2023-11-29 06:15:52 +0600

    Additional axis for graph mode, 0 and 1 Y axis markings

--- code/graph/dnr_map_plot.c
+++ code/graph/dnr_map_plot.c
@@ -19,6 +19,7 @@
  *  along with Project "Doner". If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdbool.h>
 #include <math.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -30,6 +31,7 @@
 #include "../graph/data/dnr_mmod_data.h"
 #include "../easy/data/dnr_easy_data.h"
 #include "../args/dnr_args_process.h"
+#include "../util/dnr_util_size.h"
 
 /*! \brief Bottom X axis padding */
 static const size_t dnr_map_axisx = 4;
@@ -78,9 +80,56 @@ static double * _map_plot(
 
 /*! \brief Plot the Y axis on the graph
  * \param[in] map Bitmap to use */
-void _map_axisy(struct dnr_map_type * map) {
+static void _map_axisy(struct dnr_map_type * map) {
     for (size_t y = dnr_map_axisp; y < map->height - dnr_map_axisp; y++)
         dnr_map_set(map, dnr_map_axisx, y, 1);
+}
+
+/*! \brief Draws 0 or 1 on the desired position
+ * \param[in] map Bitmap to use
+ * \param[in] px X position
+ * \param[in] py Y position
+ * \param[in] index 0 or 1 
+ * \param[in] above Draw it above proveded Y? */
+static void _map_char(
+    struct dnr_map_type * map, 
+    size_t px, 
+    size_t py, 
+    size_t index,
+    bool   above
+) {
+    static const size_t pad = 2;
+    static const unsigned char chars[2][6][4] = {
+        {   { 0, 1, 1, 0 },
+            { 1, 1, 0, 1 },
+            { 1, 1, 0, 1 },
+            { 1, 1, 0, 1 },
+            { 1, 1, 0, 1 },
+            { 0, 1, 1, 0 }},
+
+        {   { 0, 1, 1, 0 },
+            { 1, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 1, 1, 1, 1 }}
+    };
+
+    const size_t ch = DNR_SIZE(chars[0]);
+    const size_t cw = DNR_SIZE(chars[0][0]);
+
+    for (size_t y = 0; y < ch; y++) {
+        for (size_t x = 0; x < cw; x++) {
+            dnr_map_set(
+                map, 
+                px + cw + pad + x, 
+                above ?
+                    py - pad - ch + y :
+                    py + pad +  1 + y, 
+                chars[index][y][x]
+            );
+        }
+    }
 }
 
 /*! \brief Plot the X axis on the graph
@@ -97,13 +146,21 @@ void _map_axisx(
     const double mapmin = map->height * dnr_map_yt;
     const double mapmax = map->height * dnr_map_yb;
     const double height = mapmax - mapmin;
+    size_t map1;
 
-    *map0 = (height * -min) / (max - min) + mapmin;
+    *map0 = round((height * -min) / (max - min) + mapmin);
+     map1 = round((height *  1.0) / (max - min) + *map0 );
     
     /* Note, that we invert the positions as the above plot's Y grows up
      * and screen Y grows down */
-    for (size_t x = dnr_map_axisp; x < map->width - dnr_map_axisp; x++)
+    for (size_t x = dnr_map_axisp; x < map->width - dnr_map_axisp; x++) {
         dnr_map_set(map, x, map->height - *map0, 1);
+        if (x % 8 == 0)
+            dnr_map_set(map, x, map->height - map1, 1);
+    }
+
+    _map_char(map, dnr_map_axisy, map->height - *map0, 0, true );
+    _map_char(map, dnr_map_axisy, map->height - map1,  1, false);
 }
 
 /*! \brief Plots the graph, selected by args
